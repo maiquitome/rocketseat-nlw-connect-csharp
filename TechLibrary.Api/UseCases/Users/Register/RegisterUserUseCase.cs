15e6849eac11d034ccdd96aeeee445a01b59a8b2
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-02-18 13:36:37 -0300

    feat: :sparkles: add RegisterUserValidator

--- TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -7,11 +7,25 @@ public class RegisterUserUseCase
 {
     public ResponseRegisteredUserJson Execute(RequestUserJson request)
     {
-
+        Validate(request);
 
         return new ResponseRegisteredUserJson
         {
 
         };
+    }
+
+    private static void Validate(RequestUserJson request)
+    {
+        var validator = new RegisterUserValidator();
+
+        FluentValidation.Results.ValidationResult result = validator.Validate(request);
+
+        bool isInvalid = result.IsValid == false;
+
+        if (isInvalid)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+        }
     }
 }
